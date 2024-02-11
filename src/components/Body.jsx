c872a57c7066391dc417251e3a31f4f35196e8a8
Himanshu Garg <himanshugarg5480@gmail.com>
2024-02-11 22:54:33 +0530

    added login page,Navbar

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
 import Homepage from "../pages/Homepage";
+import Login from "../pages/Login";
 
 
 const Body = () => {
@@ -10,6 +11,10 @@ const Body = () => {
     {
       path: "/",
       element: <Homepage />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
     },
   ]);
   return <RouterProvider router={reactRouter} />;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <nav className="bg-[#C7C8CC]">
       <div className="w-full px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="flex gap-4">
+          <div className="flex gap-4 text-md">
             <Link to='/'>
               <span className="text-gray-700 hover:text-gray-500 font-medium">Home</span>
             </Link>
@@ -15,8 +16,8 @@ const Navbar = () => {
             </Link>
           </div>
           <div className="flex gap-1">
-            <button className="bg-[#524e4e] hover:bg-[#6f6f70] text-white p-2 rounded-lg">Login</button>
-            <button className="bg-[#524e4e] hover:bg-[#6f6f70] text-white shadow-sm shadow-[#E3E1D9] p-2 rounded-lg">Signup</button>
+            <button onClick={()=>{navigate('/login')}} className="bg-[#524e4e] hover:bg-[#6f6f70] text-white p-2 rounded-lg">Login</button>
+            <button onClick={()=>{navigate('/signup')}} className="bg-[#524e4e] hover:bg-[#6f6f70] text-white shadow-sm shadow-[#E3E1D9] p-2 rounded-lg">Signup</button>
           </div>
         </div>
       </div>
